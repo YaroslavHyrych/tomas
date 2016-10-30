b72c853b809d7486cd7c8347d2b56eb45ab27203
Yaroslav Hyrych <yaroslavhyrych@gmail.com>
2016-10-30 11:21:42 +0200

    Fix activity duration

--- app/scripts/directives/activities.js
+++ app/scripts/directives/activities.js
@@ -11,10 +11,14 @@ angular.module('tomasApp')
     return {
       templateUrl: 'views/activities.html',
       restrict: 'E',
-      controllerAs: 'ctrl',
+      controllerAs: 'activitiesCtrl',
       controller: ['$scope', 'history', function($scope, history) {
         function loadToScope() {
           $scope.activities = history.load().reverse();
+        }
+
+        function timeFormat(time) {
+          return time.toString().length == 1 ? '0' + time : time;
         }
 
         loadToScope();
@@ -25,11 +29,11 @@ angular.module('tomasApp')
 
         this.makeDuration = function (activity) {
           var date = new Date(activity.stopDate - activity.startDate),
-            hours = date.getHours(),
-            minutes = date.getMinutes(),
-            seconds = date.getSeconds();
+            hours = timeFormat(date.getHours() + date.getTimezoneOffset()/60),
+            minutes = timeFormat(date.getMinutes()),
+            seconds = timeFormat(date.getSeconds());
 
-          return hours + ':' + minutes + ':' + seconds;
+          return ((hours == '00') ? '' : hours + ':') +  minutes + ':' +  seconds;
         };
       }]
       // link: function postLink(scope, element, attrs) {
